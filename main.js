f7f1c8786943d3e847eb041d7d7f53fb5708c6be
arerimr <raydelys.morrobelr@gmail.com>
2022-12-13 20:42:21 -0500

    Events Lab

--- main.js
+++ main.js
@@ -7,3 +7,27 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+const palCol = document.querySelectorAll('#palette')
+//let curr
+for (let col of palCol){
+  col.addEventListener('click', x => {
+    let curr = document.querySelector('#current-color')
+    //  color = col.style.background
+    // if (color){
+    //   curr.style.background = color
+    // }
+    // curr.style.background = col.style.background
+    curr.style.background = x.target.style.background
+    // curr.setAttribute('style', 'background-color', color)
+
+  })
+}
+
+const cells = document.querySelectorAll('.cell')
+
+for (let cell of cells){
+  cell.addEventListener('click', x => {
+    let curr = document.querySelector('#current-color')
+    x.target.style.background = curr.style.background
+  })
+}
